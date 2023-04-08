840185f6660f16c66e4a42931a42037fd03d14a6
shlyzhas <8willox8@gmail.com>
2023-04-08 20:59:52 +0300

    simple server application

---
+++ README.md
@@ -0,0 +1,6 @@
+### :computer: Simple Server Application
+An application that returns the current date in format 01.02.2006<br/>
+Run the server with command: go run . and click on the link /status
+
+### Go version
+1.20
--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Hi!")
+	fmt.Print("...Server running")
+	server := echo.New()
+
+	server.GET("/status", Handler)
+
+	err := server.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Handler(ctx echo.Context) error {
+	data := time.Now().Format("02.01.2006")
+	err := ctx.String(http.StatusOK, data)
+	if err != nil {
+		return err
+	}
+	return nil
 }
